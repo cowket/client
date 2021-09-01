8fbb047ffdae6abfc4cf2998dd532df2e79d70cb
hyewon do <tsc03018@gmail.com>
2021-09-01 23:43:08 +0900

    env: setting development environment

---
+++ pages/register.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+const Register: NextPage = () => {
+  return <div className={styles.container}>회원가입 페이지</div>;
+};
+
+export default Register;
