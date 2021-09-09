84bd84195282bce248139ce7d66858410f2aaa3a
hyewon do <tsc03018@gmail.com>
2021-09-09 23:30:40 +0900

    chore: pages 파일 삭제

--- pages/login.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import { LoginForm } from "components/LoginForm";
-
-const Login: NextPage = () => {
-  return <LoginForm />;
-};
-
-export default Login;
--- pages/register.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import { RegisterForm } from "components/RegisterForm";
-
-const Register: NextPage = () => {
-  return <RegisterForm />;
-};
-
-export default Register;
