ff85a981c03d0eb73d59402111938932e33248c1
hyewon do <tsc03018@gmail.com>
2021-10-16 22:39:20 +0900

    feat: 채널 검색 모달 추가

--- src/components/Chat/ListBox/index.tsx
+++ src/components/Chat/ListBox/index.tsx
@@ -4,17 +4,21 @@ import Channel from 'components/Chat/List/Channel';
 import { getTeamParticipants } from 'api/team';
 import { getChannel } from 'api/channel';
 import ChannelContext from 'context/channel';
+import userContext from 'context/user';
 import selectContext from 'context/select';
 import './style.scss';
 
 const ListBox = () => {
   const [teamDMList, setTeamDMList] = useState<TeamParticipant[]>([]);
   const [channelList, setChannelList] = useState<Channel[]>([]);
+  const { userInfo } = useContext(userContext);
   const { selectedTeam, setSelectedChannel } = useContext(selectContext);
+
   useEffect(() => {
     if (selectedTeam) {
       getTeamParticipants(selectedTeam.uuid).then((res) => {
-        setTeamDMList(res);
+        const list = res?.filter((person) => person.uuid !== userInfo?.uuid);
+        setTeamDMList(list);
       });
       getChannel(selectedTeam.uuid).then((res) => {
         if (res.length > 0) {
---
+++ src/components/Chat/SearchChannel/index.tsx
@@ -0,0 +1,68 @@
+import useDesktopSize from 'hooks/useDesktopSize';
+import React, { useState, useContext } from 'react';
+import { postChannel } from 'api/channel';
+import selectContext from 'context/select';
+import channelContext from 'context/channel';
+import { CloseOutlined, ArrowBack } from '@material-ui/icons';
+import {
+  IconButton,
+  Button,
+  InputAdornment,
+  TextField,
+} from '@material-ui/core';
+import './style.scss';
+
+type SearchChannelProps = {
+  onClose(): void;
+};
+
+const SearchChannel = ({ onClose }: SearchChannelProps) => {
+  const [channelName, setChannelName] = useState<string>();
+  const isDesktopSize = useDesktopSize();
+  const { setChannelList, channelList } = useContext(channelContext);
+  const { selectedTeam } = useContext(selectContext);
+
+  const onSubmit = async () => {
+    if (channelName && selectedTeam?.uuid) {
+      postChannel({ team_uuid: selectedTeam.uuid, name: channelName }).then(
+        (res) => setChannelList([...channelList, res])
+      );
+    }
+    onClose();
+  };
+
+  return (
+    <div className="modalWrapper">
+      <div className="searchModalContainer">
+        <header>
+          <div className="title">
+            {!isDesktopSize && (
+              <IconButton
+                size="small"
+                onClick={onClose}
+                style={{ marginRight: '10px' }}
+              >
+                <ArrowBack fontSize="small" />
+              </IconButton>
+            )}
+            모든 채널 탐색
+          </div>
+          {isDesktopSize && (
+            <IconButton size="small" onClick={onClose}>
+              <CloseOutlined fontSize="small" />
+            </IconButton>
+          )}
+        </header>
+        <desc>모든 공개채널 중 참여하고 싶은 채널에 가입하세요.</desc>
+        <section>
+          <div className="channel">
+            <p>ddddd</p>
+            <Button>참여</Button>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default SearchChannel;
